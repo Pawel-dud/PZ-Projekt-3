e4329357af8f5044ea0086315beec5c32f355565
Pawel-dud <pawel_dudziak@spoko.pl>
2024-11-27 17:20:49 +0100

    wyswietlanie tablicy nieposortowaniej

--- main.cpp
+++ main.cpp
@@ -1,2 +1,14 @@
 #include "mergesort.h"
-#include <iostream>
+
+int main() {
+    MergeSort MS;
+
+    int tab[] = { 3, -4, 24, 0, 50, 17, -1, 108};
+    int rozmiar = sizeof(tab) / sizeof(tab[0]);
+
+    cout << "Tablica przed sortowaniem: ";
+    for (int i = 0; i < rozmiar; i++) {
+        cout << tab[i] << " ";
+    }
+    cout << endl;
+}
