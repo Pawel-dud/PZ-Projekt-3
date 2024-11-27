6afac2d18c543a4f7feaedd8f33f35f93f650b10
Pawel-dud <pawel_dudziak@spoko.pl>
2024-11-27 17:38:04 +0100

    sortownie

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,2 +1,16 @@
-#include <iostream>
 #include "mergesort.h"
+
+void MergeSort::start(int tab[], int rozmiar) {
+    sortowanie(tab, 0, rozmiar - 1);
+}
+
+void MergeSort::sortowanie(int tab[], int lewa, int prawa) {
+    if (lewa < prawa) {
+        int srodek = lewa + (prawa - lewa) / 2;
+
+        sortowanie(tab, lewa, srodek);
+        sortowanie(tab, srodek + 1, prawa);
+
+        scalanie(tab, lewa, srodek, prawa);
+    }
+}
